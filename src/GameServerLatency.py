40cd03b79631fd4c83a72286d00ef7c1ab913f6c
Polina <polina_cherkasova@outlook.com>
2024-03-24 14:08:17 +0200

    lab 6 completed

--- src/GameServerLatency.py
+++ src/GameServerLatency.py
@@ -1,30 +1,44 @@
 # Рівень 3
 # Варіант 1
-import queue
+
+# Вхiднi данi
+# • Перший рядок мiстить N i M — кiлькiсть вузлiв та з’єднань вiдповiдно
+# • Другий рядок мiстить перелiк цiлих чисел, роздiлених пробiлом — номери
+#   вузлiв, якi є клiєнтами. Усi вузли в мережi нумеруються вiд 1 до N
+# • Наступнi M рядкiв мiстять трiйки натуральних чисел startnode, endnode, latency
+# — номер початкового вузла, кiнцевого вузла та затримка для кожного з’єднання
+#
+# Вихiднi данi
+# Одне число — мiнiмальне значення найбiльшої затримки до клiєнта
+# (яке ми отримаємо при оптимальному розташуваннi сервера).
+
+import heapq
 
 
-def shortest_paths_to_clients(graph: {int: [(int, int)]}, start_point: int) -> [int]:
+def shortest_paths_to_clients(
+    graph: {int: [(int, int)]}, start_point: int, clients: [int]
+) -> [int]:
     visited_nodes = set()
     distances_to_vertexes = {key: int(1e10) for key in graph.keys()}
     distances_to_vertexes[start_point] = 0
-    pq = queue.PriorityQueue()
-    pq.put((start_point, distances_to_vertexes[start_point]))
-    while pq:
-        vertex, dist_to_vertex = pq.get()
+    pq = []
+    heapq.heapify(pq)
+    heapq.heappush(pq, (distances_to_vertexes[start_point], start_point))
+    while not len(pq) == 0:
+        dist_to_vertex, vertex = heapq.heappop(pq)
         if vertex in visited_nodes:
             continue
         visited_nodes.add(vertex)
 
-        for edges in graph[vertex]:
-            for edge_num in range(len(edges)):
-                vertex2, latency_v1v2 = edges[edge_num]
-                """
-                TypeError: cannot unpack non-iterable int object
-                """
-                if dist_to_vertex + latency_v1v2 < distances_to_vertexes[vertex2]:
-                    distances_to_vertexes[vertex2] = dist_to_vertex + latency_v1v2
-                    pq.put(vertex2, distances_to_vertexes[vertex2])
-    return distances_to_vertexes
+        for vertex2, latency_v1v2 in graph[vertex]:
+            if dist_to_vertex + latency_v1v2 < distances_to_vertexes[vertex2]:
+                distances_to_vertexes[vertex2] = dist_to_vertex + latency_v1v2
+                heapq.heappush(pq, (distances_to_vertexes[vertex2], vertex2))
+    dist_to_clients = []
+    for item in range(1, len(distances_to_vertexes) + 1):
+        if item in clients:
+            dist_to_clients.append(distances_to_vertexes[item])
+    return dist_to_clients
 
 
 def find_server_position(
@@ -32,7 +46,7 @@ def find_server_position(
 ) -> int:
     if v and e and clients_list and connections:
         routers = set(x for x in range(1, v + 1))
-        routers.difference(clients_list)
+        routers.difference_update(clients_list)
         list_view = {}
         for startnode, endnode, latency in connections:
             if startnode not in list_view.keys():
@@ -41,9 +55,17 @@ def find_server_position(
                 list_view[endnode] = []
             list_view[startnode].append((endnode, latency))
             list_view[endnode].append((startnode, latency))
-        max_latency = 0
+        max_routers_latency = []
         for router in routers:
-            max_latency = max(shortest_paths_to_clients(list_view, router), max_latency)
-        return max_latency
+            max_routers_latency.append(
+                max(shortest_paths_to_clients(list_view, router, clients_list))
+            )
+        return min(max_routers_latency)
     else:
         return -1
+
+
+v, e = 3, 2
+clients = [1, 3]
+connections = [[1, 2, 50], [2, 3, 1000000000]]
+print(find_server_position(v, e, clients, connections))
