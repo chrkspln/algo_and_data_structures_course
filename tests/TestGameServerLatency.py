0892abe637b8c6c5bb9dd39d917c6b551079b320
Polina <polina_cherkasova@outlook.com>
2024-03-26 13:07:50 +0200

    moving txt files into resources dir

--- tests/TestGameServerLatency.py
+++ tests/TestGameServerLatency.py
@@ -4,19 +4,19 @@ from src.GameServerLatency import *
 
 class TestGameServerLatency(unittest.TestCase):
     def test_normal_case(self):
-        file = "../src/gamsrv_in1.txt"
+        file = "../src/resources/gamsrv_in1.txt"
         out = "../src/gamsrv_out1.txt"
         find_server_position(file, out)
-        with open("../src/gamsrv_out1.txt") as output_file:
+        with open("../src/resources/gamsrv_out1.txt") as output_file:
             output = output_file.read()
         expected = "100"
         self.assertEqual(output, expected)
 
     def test_empty_input(self):
-        file = "../src/gamsrv_in2.txt"
+        file = "../src/resources/gamsrv_in2.txt"
         out = "../src/gamsrv_out2.txt"
         find_server_position(file, out)
-        with open("../src/gamsrv_out2.txt") as output_file:
+        with open("../src/resources/gamsrv_out2.txt") as output_file:
             output = output_file.read()
         expected = "-1"
         self.assertEqual(output, expected)
